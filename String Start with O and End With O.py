f5045c8defde93b29102377e5c08b37f319baa1c
Virendra Kalwar <vkalwar132000@gmail.com>
2023-11-07 22:59:52 +0530

    Create String Start with O and End With O.py

---
+++ String Start with O and End With O.py
@@ -0,0 +1,6 @@
+strtext = "Oppo"
+last_index = len(strtext) - 1
+if strtext[0] == "O" and strtext[last_index] == "o":
+    print("String Start With O & End With O")
+else:
+    print("String Does not Start With O & End With O")
